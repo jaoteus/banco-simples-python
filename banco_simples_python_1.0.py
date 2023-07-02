ba025ab130b356273641b808ab970937023f8a0c
Mateus <mateusff2323@gmail.com>
2023-07-02 11:40:20 -0300

    Correção ortógráfica e comentários

--- banco_simples_python_1.0.py
+++ banco_simples_python_1.0.py
@@ -9,16 +9,16 @@ LEIA:
     Conforme eu vá adquirindo conhecimentos, irei estar sempre atualizando o código.
     Portanto, se encontar algum erro, me avise, estarei agradecendo :)
 '''
-#Variáveis
+#Variáveis necessárias
 saldo_cc = 0.0
 saldo_cp = 0.0
 valor = 0.0 #--> usado para fazer depósito e saque
 nome_titular = 'Advogado José David Gil Rodrigues'
 senha = 'alunoete123' # --> por enquanto irá ficar fixa, apenas para teste.
 cpf = '000.000.000-11'
-numero_conta_corrente = 1234-1 #--> por enquanto irá ficar fixa, apenas para teste.
-numero_conta_poupanca = 1234-2 #--> por enquanto irá ficar fixa, apenas para teste.
-agencia = 2332-9 #--> por enquanto irá ficar fixa, apenas para teste.
+numero_conta_corrente = '1234-1' #--> por enquanto irá ficar fixa, apenas para teste.
+numero_conta_poupanca = '1234-2' #--> por enquanto irá ficar fixa, apenas para teste.
+agencia = '2332-9' #--> por enquanto irá ficar fixa, apenas para teste.
 endereco = 'Rua Seilá, n°00, Recife, Pernambuco.' #--> por enquanto irá ficar fixa, apenas para teste.
 numero = '+55 00 0000-0000' #--> por enquanto irá ficar fixa, apenas para teste.
 opcao_menu = ''
@@ -51,7 +51,7 @@ def dados_da_conta():
 
 def deposito_cc():
     global valor, saldo_cc, loop_deposito, opcao_menu
-    valor = float(input('Digite o valor desejado para depósito: '))
+    valor = float(input('Digite o valor desejado para depósito: R$'))
     if valor >= 0.0:
         saldo_cc = saldo_cc + valor
         print('Valor depositado com sucesso!')
@@ -65,7 +65,7 @@ def deposito_cc():
 
 def saque_cc():
     global saldo_cc, valor
-    valor = float(input('Digite o valor para o saque: '))
+    valor = float(input('Digite o valor para o saque: R$'))
     if valor > saldo_cc:
         print('Saldo insuficiente!')
         valor = 0.0
@@ -77,21 +77,21 @@ def saque_cc():
 
 def deposito_cp():
     global valor, saldo_cp, saldo_cc
-    valor = float(input('Digite o  valor para adicionar na conta poupança: '))
+    valor = float(input('Digite o  valor para adicionar na conta poupança: R$'))
     if valor <= saldo_cc:
         saldo_cc = saldo_cc - valor
         saldo_cp = saldo_cp + valor
-        print('Valor adidionado com sucesso!')
+        print('Valor adicionado com sucesso!')
         valor = 0.0
     elif valor > saldo_cc:
-        print('Saldo insuficiente na conta corrente')
+        print('Saldo insuficiente na conta corrente!')
         valor = 0.0
     else:
         print('')
 
 def saque_cp():
     global valor, saldo_cp, saldo_cc
-    valor = float(input('Digite o valor para sacar na conta corrente: '))
+    valor = float(input('Digite o valor para sacar na conta corrente: R$'))
     if valor > saldo_cp:
         print('Saldo insuficiente!')
         valor = 0.0
@@ -103,22 +103,21 @@ def saque_cp():
 #Aba de login
 while loop_login:
     email_ou_cpf = str(input('Digite seu e-mail ou CPF: '))
-    senha = str(input('Digite sua senha: '))
+    senha_login = str(input('Digite sua senha: '))
 
-    if email_ou_cpf == 'alunoete123@gmail.com' or email_ou_cpf == '000.000.000-11' and senha == 'alunoete123':
+    # Se todos forem corretos
+    if (email_ou_cpf == email or email_ou_cpf == cpf) and senha_login == senha:
         print('Login efetuado com sucesso!')
         loop_login = False
 
-    elif email_ou_cpf != 'alunoete123@gmail.com' or email_ou_cpf != '000.000.000-11' and senha != 'alunoete123':
+    # Se todos forem incorretos
+    elif (email_ou_cpf != email or email_ou_cpf != cpf) and senha_login != senha:
         print('Login não efetuado, tente novamente!')
         continue
 
-    elif email_ou_cpf == 'alunoete123@gmail.com' or email_ou_cpf == '000.000.000-11' and senha != 'alunoete123':
-        print('Login não efetuado, tente novamente!')
-        continue
-
+    #Se nenhum dos dois forem satisfeitos:
     else:
-        print('')
+        print('Login não efetuado, tente novamente!')
 #Menu
 while loop_principal:
     menu_principal()
